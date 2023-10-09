179386a9e9305c71d4e04375f6a2a7b1e0b5fc24
Salma Mamdoh <52897508+Salma-Mamdoh@users.noreply.github.com>
2023-10-09 23:13:36 +0300

    Create Weather Observation Station 5.sql

---
+++ HackerRank Problems/Weather Observation Station 5.sql
@@ -0,0 +1,8 @@
+select city, length(city) from station
+where length(city) = (select min(length(city)) from station)
+order by 1 asc limit 1
+;
+select city, length(city) from station
+where length(city) = (select max(length(city)) from station)
+order by 1 asc limit 1
+;
